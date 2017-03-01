88f8a8d9b74ed831570f57a5fb81848d1fca7083
zach fine <zfine@utexas.edu>
2017-03-01 17:12:18 -0600

    Rework views

---
+++ app/Security/LoginAuthenticator.php
@@ -0,0 +1,87 @@
+<?php namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+
+class LoginAuthenticator extends AbstractGuardAuthenticator
+{
+    private $encoderFactory;
+
+    public function __construct(EncoderFactoryInterface $encoderFactory)
+    {
+        $this->encoderFactory = $encoderFactory;
+    }
+
+    public function getCredentials(Request $request)
+    {
+        if($username = $request->request->get('username') && $secret = $request->request->get('password')) {
+            return array(
+                'username' => $username,
+                'secret' => $secret,
+            );
+        }
+
+        return;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        return $userProvider->loadUserByUsername($credentials['username']);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // check credentials - e.g. make sure the password is valid
+        // return true to cause authentication success
+
+        $encoder = $this->encoderFactory->getEncoder($user);
+
+        return $encoder->isPasswordValid(
+            $user->getPassword(),
+            $credentials['secret'],
+            $user->getSalt()
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        // on success, let the request continue
+        return;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $data = array(
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData()),
+
+            // or to translate this message
+            // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
+        );
+
+        return new JsonResponse($data, 403);
+    }
+
+    /**
+     * Called when authentication is needed, but it's not sent
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        $data = array(
+            // you might translate this message
+            'message' => 'Authentication Required',
+        );
+
+        return new JsonResponse($data, 401);
+    }
+
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}
